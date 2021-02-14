2236cd0031d26360229ef6917fcc8073e277995c
nivek159 <67131545+nivek159@users.noreply.github.com>
2021-02-13 22:28:43 -0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+// Created by Madhav
+
+var is_visible = false ;
+
+function check()
+{
+
+    var input = document.getElementById("password").value;   
+    input=input.trim();
+    document.getElementById("password").value=input;
+    if(input.match("ATICUN"))
+    {
+    	alert("Felicidades, lo lograste!!");
+        location.href ="";
+    }
+}
+
